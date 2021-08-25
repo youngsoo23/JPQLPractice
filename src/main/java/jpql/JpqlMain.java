9599e8b0a6202b86af754db89f597c2d9a7ce025
youngsoooh <oh.youngsoo23@gmail.com>
2021-08-25 17:07:01 +0900

    프로젝션

--- src/main/java/jpql/JpqlMain.java
+++ src/main/java/jpql/JpqlMain.java
@@ -25,15 +25,29 @@ public class JpqlMain {
 //            query.setParameter("username", "member1");
 //            Member sr = query.getSingleResult();
 
-            Member member1 = em.createQuery("select m from Member m where m.username =:username", Member.class)
-                    .setParameter("username", "member1")
-                            .getSingleResult();
+//            Member member1 = em.createQuery("select m from Member m where m.username =:username", Member.class)
+//                    .setParameter("username", "member1")
+//                            .getSingleResult();
+//
+//            System.out.println("member1 = " + member1.getUsername());
 
-            System.out.println("member1 = " + member1.getUsername());
+
+//            //Object[] 타입으로 조회
+//            List rl= em.createQuery("select m.username, m.age from Member m").getResultList();
+//
+//            Object o = rl.get(0);
+//            Object[] result = (Object[]) o;
+//            System.out.println("username = " + result[0]);
+//            System.out.println("age = " + result[1]);
+
+        // new 명령어로 조회
+            //단순 값을 DTO로 바로 조회 
+            //SELECT new jpabook.jpql.UserDTO(m.username, m.age) FROM Member m
+            List<MemberDto> resultList =
+                    em.createQuery("select new jpql.MemberDto(m.username,m.age) from Member m", MemberDto.class).getResultList();
+
 
             tx.commit();
-
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
